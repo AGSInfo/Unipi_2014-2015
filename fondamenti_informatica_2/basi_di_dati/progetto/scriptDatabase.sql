a2d416ac04d62e5f934e7292ad1664f1375f1e71
Gianluca Mondini <zapateo@live.it>
2015-07-08 08:15:06 +0200

    Aggiornato script generazione database

--- fondamenti_informatica_2/basi_di_dati/progetto/scriptDatabase.sql
+++ fondamenti_informatica_2/basi_di_dati/progetto/scriptDatabase.sql
@@ -2,10 +2,43 @@ DROP DATABASE IF EXISTS ProgettoRistoranti;
 
 CREATE DATABASE ProgettoRistoranti;
 
+USE ProgettoRistoranti;
+
+--------------------------------------------------------------------------------
+-- CREAZIONE TABELLE
+--------------------------------------------------------------------------------
+
 CREATE TABLE Sede (
       IdSede INT,
       Via VARCHAR(20),
       nCivico INT,
       Citta VARCHAR(20),
       PRIMARY KEY (IdSede)
+);
+
+CREATE TABLE Magazzino (
+      IdMagazzino INT,
+      IdSede INT,
+      PRIMARY KEY (IdSede)
+);
+
+CREATE TABLE Scaffale (
+      IdScaffale INT,
+      IdMagazzino INT,
+      PRIMARY KEY (IdScaffale)
+);
+
+CREATE TABLE Confezione (
+      IdConfezione INT,
+      Peso FLOAT,
+      PrezzoAcquisto FLOAT,
+      DataAcquisto DATE,
+      DataConsegna DATE,
+      DataScadenza DATE,
+      Aspetto INT, --> ???
+      Stato INT, --> ???
+      QuantitàRimanente FLOAT, --> ???
+      Ingrediente INT,
+      Scaffale INT,
+      PRIMARY KEY (IdConfezione)
 );
